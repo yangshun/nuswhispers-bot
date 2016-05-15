5c610546c914d2f7409bce717e309f20d006b4fc
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-05-15 23:19:23 +0800

    Read updates from bot

--- index.go
+++ index.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+  "net/http"
+  "io/ioutil"
+  "encoding/json"
+)
+
+type config struct {
+  TelegramBotAPI string
+  TelegramBotToken string
+}
 
 func main() {
-  fmt.Println("Hello, 世界")
+  file, e := ioutil.ReadFile("./config.json")
+  if e != nil {
+    fmt.Printf("File error: %v\n", e)
+    os.Exit(1)
+  }
+  fmt.Printf("%s\n", string(file))
+
+  var appConfig config
+  json.Unmarshal(file, &appConfig)
+
+  response, err := http.Get(appConfig.TelegramBotAPI + appConfig.TelegramBotToken + "/getUpdates")
+  if err != nil {
+    fmt.Printf("%s", err)
+  } else {
+    defer response.Body.Close()
+    contents, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+      fmt.Printf("%s", err)
+    }
+    fmt.Printf("%s\n", string(contents))
+  }
 }
